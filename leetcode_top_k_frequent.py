d59fb933533868555d0b9e283eaa49e494c1dac1
Vadim Miroshnik <58141283+fall3nangel@users.noreply.github.com>
2023-06-03 13:03:48 +0300

    Top K Frequent Elements

---
+++ leetcode_top_k_frequent.py
@@ -0,0 +1,52 @@
+# 347. Top K Frequent Elements
+# Given an integer array nums and an integer k, return the k most frequent elements. 
+# You may return the answer in any order.
+# Example 1:
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+# Example 2:
+# Input: nums = [1], k = 1
+# Output: [1]
+# Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+# https://leetcode.com/problems/top-k-frequent-elements/
+from operator import itemgetter
+
+
+class Solution:
+    def topKFrequent1(self, nums: list[int], k: int) -> list[int]:
+        freq = {}
+        result = []
+        for i in nums:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+        res = dict(sorted(freq.items(), key=itemgetter(1)))
+        for i in list(res.keys())[:-k-1:-1]:
+            result.append(i)
+        return result
+
+    def topKFrequent2(self, nums: list[int], k: int) -> list[int]:
+        """" O(n) solution - Bucket Sort"""
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+sol = Solution()
+res = sol.topKFrequent2([1, 1, 1, 2, 2, 3], 2)
+print(res)
+
+res = sol.topKFrequent2([1], 1)
+print(res)
