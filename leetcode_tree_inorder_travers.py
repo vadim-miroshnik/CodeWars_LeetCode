743cdd04f7a7e6bf3d957b37db3469849e03a629
Vadim Miroshnik <58141283+fall3nangel@users.noreply.github.com>
2023-06-10 16:21:15 +0300

    Binary Tree Inorder Traversal

---
+++ leetcode_tree_inorder_travers.py
@@ -0,0 +1,59 @@
+# 94. Binary Tree Inorder Traversal
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def to_binary_tree(items: list[int]) -> TreeNode:
+    """Create BT from list of values."""
+    n = len(items)
+    if n == 0:
+        return None
+
+    def inner(index: int = 0) -> TreeNode:
+        if n <= index or items[index] is None:
+            return None
+        node = TreeNode(items[index])
+        node.left = inner(index + 1)
+        node.right = inner(index + 2)
+        return node
+
+    return inner()
+
+
+class Solution:
+    def inorderTraversal1(self, root: TreeNode | None) -> list[int]:
+        """my first solution - recursive"""
+        res = []
+        if root:
+            if root.left:
+                res = self.inorderTraversal(root.left)
+            res.append(root.val)
+            if root.right:
+                res.extend(self.inorderTraversal(root.right))
+        return res
+
+    def inorderTraversal2(self, root: TreeNode | None) -> list[int]:
+        """Neetcode solution - iterative"""
+        res, stack = [], []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
+
+
+sol = Solution()
+bt = to_binary_tree([1, None, 2, 3])
+print(sol.inorderTraversal2(bt))
